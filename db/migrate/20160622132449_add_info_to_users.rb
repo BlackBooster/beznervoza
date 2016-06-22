de0f96c56da8f55f38c549bc1fad825eaa4fe406
klim <clementii.cicali@gmail.com>
2016-06-22 16:48:06 +0300

    Update 20160622132449_add_info_to_users.rb
    Add additional fields for last and first name

--- db/migrate/20160622132449_add_info_to_users.rb
+++ db/migrate/20160622132449_add_info_to_users.rb
@@ -1,6 +1,7 @@
 class AddInfoToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :name, :string
+    add_column :users, :last_name, :string, :limit => 50
+    add_column :users, :first_name, :string, :limit => 50
     add_column :users, :date_of_birth, :datetime
     add_column :users, :is_female, :boolean, default: false
   end
