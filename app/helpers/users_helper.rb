be85bd53776a20677f63ca46a59f53e1ae473209
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-05 11:06:29 +0300

    Add gender helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def full_adress
     @user.street + " " + @user.house + " " + @user.apartment + " " + @user.zipcode
   end
+
+  def gender
+    @gender = User::GENDER_TYPES.to_h.key(@user.is_female)
+
+  end
 end
