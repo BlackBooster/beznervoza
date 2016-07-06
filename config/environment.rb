bbf8f79df7efae52b2ee03c7b42aa31e06a2a17f
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-06 13:48:57 +0300

    Fix env

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'blackbooster@inbox.ru',
-  :password => 'gsyt450@!@',
-  :domain => 'beznevroza.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'email',
+#   :password => 'pwd',
+#   :domain => 'yourdomain.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
