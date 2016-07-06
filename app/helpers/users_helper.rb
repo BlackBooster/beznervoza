f9a6e18e8c684328d973796505f6f02a77059062
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-06 11:28:36 +0300

    Revert "Add gender helper"
    This reverts commit be85bd53776a20677f63ca46a59f53e1ae473209.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,4 @@ module UsersHelper
   def full_adress
     @user.street + " " + @user.house + " " + @user.apartment + " " + @user.zipcode
   end
-
-  def gender
-    @gender = User::GENDER_TYPES.to_h.key(@user.is_female)
-
-  end
 end
