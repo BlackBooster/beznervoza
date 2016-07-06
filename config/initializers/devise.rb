a14ec22bfcdd4a686656dde7e4005ebce676ebdc
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-06 16:43:56 +0300

    Add reconfirmable option

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,6 +40,7 @@ Devise.setup do |config|
   # The same considerations mentioned for authentication_keys also apply to request_keys.
   # config.request_keys = []
   config.mailer_sender = "beznevroza@heroku.com"
+  config.reconfirmable = true
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
