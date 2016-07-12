5e9f584350c2bc3aa08d9bdd7c1df9770f24bd3e
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-12 11:13:05 +0300

    Fix redirect after sign up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def welcome
+    unless params[:new]
+      redirect_to root_path
+    end
   end
 
   def show
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   def after_sign_up_path_for(resource)
-    welcome_path(resource)
+    welcome_path(new: 1)
   end
 
   def after_inactive_sign_up_path_for(resource)
-    welcome_path(resource)
+    welcome_path(new: 1)
   end
 end
