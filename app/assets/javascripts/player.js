e4978ce7481e08cad7a65a124173d0fbba3bd0bf
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-14 17:38:47 +0300

    Fix player

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     ready: function () {
       $(this).jPlayer("setMedia", {
         title: "Mos def",
-        mp3: "https://s3.eu-central-1.amazonaws.com/blackbooster/audios/Massive+Attack+feat.+Mos+Def+-+I+Against+I.mp3"
+        mp3: link
       });
     },
     cssSelectorAncestor: "#jp_container_1",
@@ -16,4 +16,7 @@ $(document).ready(function(){
     remainingDuration: false,
     toggleDuration: true
   });
+ var link =  "https://s3.eu-central-1.amazonaws.com/blackbooster/audios/Massive+Attack+feat.+Mos+Def+-+I+Against+I.mp3"
+  $('#jp_audio_0[src=""]').hide()
+  $('#jp_audio_0:not[src=""]').show()
 });
