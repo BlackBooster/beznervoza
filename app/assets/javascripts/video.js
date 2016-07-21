55e657cba8ee315f5a6b902341146d9c6c2eceda
Ivan Popritsak <ivan.popritsak@gmail.com>
2016-07-21 19:43:56 +0300

    Fix some player's scripts

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     videoVolume: 'horizontal',
     features: ['playpause','progress','volume','fullscreen']
   });
+  $('#video-player').bind('contextmenu', function() { return false; });
+  $(document).mousedown(function(e){
+    if( e.button == 2 ) {
+      $('#video-player').removeAttr('src');
+      return false;
+    }
+    return true;
+  });
 });
